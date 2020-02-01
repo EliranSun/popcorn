11f9c6d145aabc74363c59615214aeab09cf0491
RyanMcklain <piro27@gmail.com>
2020-02-01 18:32:06 +0200

    Add files via upload

---
+++ precache-manifest.4e1a803978b891db20f5957915aec249.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b145c33ad87ef81302f15f73b5fc3e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c46a3d3c6acc9d96325",
+    "url": "/static/css/main.0dccc80d.chunk.css"
+  },
+  {
+    "revision": "757545820e625d5e620e",
+    "url": "/static/js/2.e3536739.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e3536739.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c46a3d3c6acc9d96325",
+    "url": "/static/js/main.384139c7.chunk.js"
+  },
+  {
+    "revision": "c22861c896676c46d628",
+    "url": "/static/js/runtime-main.0875dc58.js"
+  },
+  {
+    "revision": "2cf30ffdbfa3db621d303e9575ff9e47",
+    "url": "/static/media/earth.2cf30ffd.gif"
+  },
+  {
+    "revision": "007306f1ac270ed393c1ca792661cb77",
+    "url": "/static/media/happy-people-lib.007306f1.jpg"
+  },
+  {
+    "revision": "e028ac5fbb700a5db916af575672e954",
+    "url": "/static/media/salad-making.e028ac5f.gif"
+  }
+]);
